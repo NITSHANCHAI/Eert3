0c8c9f9dfeca419c5cee66a6f81bf63e70dba152
NITSHANCHAI <nitshanchainitchin@gmail.com>
2025-06-27 00:29:13 +0800

    Add files via upload

---
+++ swipe.js
@@ -0,0 +1,13 @@
+let touchStartX = 0;
+let touchEndX = 0;
+
+document.addEventListener('touchstart', e => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+document.addEventListener('touchend', e => {
+  touchEndX = e.changedTouches[0].screenX;
+  if (touchStartX - touchEndX > 50) {
+    window.location.href = "philosophy.html";
+  }
+});
